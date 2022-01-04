9cf468a248dea1635531bc0b7b37c818a803d70c
colorbox <colorbox222@gmail.com>
2022-01-04 23:39:33 +0900

    Add main logic

--- lib/anubis.rb
+++ lib/anubis.rb
@@ -3,6 +3,15 @@
 require_relative "anubis/version"
 
 module Anubis
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.judge_at_end(config, ignore_models: [])
+    config.after type: :feature do
+      ApplicationRecord.subclasses.reject {|klass| klass.abstract_class }.all? {|klass|
+        next if ignore_models.include? klass
+
+        klass.all.each {|obj|
+          expect(obj).to be_valid
+        }
+      }
+    end
+  end
 end

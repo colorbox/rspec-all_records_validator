baf4700209dd9db8deca31873fe766b1abef1c96
colorbox <colorbox222@gmail.com>
2022-01-11 22:59:25 +0900

    Refactor main code

--- lib/rspec_all_record_validator_after_system_spec.rb
+++ lib/rspec_all_record_validator_after_system_spec.rb
@@ -5,13 +5,13 @@ require_relative "rspec_all_record_validator_after_system_spec/version"
 module RSpecAllRecordvalidatorAfterSystemSpec
   def self.judge_at_end(config, ignore_models: [], type: :system)
     config.after type: type do
-      ApplicationRecord.subclasses.reject {|klass| klass.abstract_class }.all? {|klass|
-        next if ignore_models.include? klass
+      target_classes = ApplicationRecord.subclasses.reject {|klass| klass.abstract_class? || ignore_models.include?(klass) }
 
-        klass.all.each {|obj|
+      target_classes.each do |klass|
+        klass.all.each do |obj|
           expect(obj).to be_valid
-        }
-      }
+        end
+      end
     end
   end
 end
